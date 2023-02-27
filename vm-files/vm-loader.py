d468a7164db101cd2a8fc22d9d7174779aa6b1fb
icyx0 <126398994+icyx0@users.noreply.github.com>
2023-02-27 09:19:20 -0500

    Update vm-loader.py

--- vm-files/vm-loader.py
+++ vm-files/vm-loader.py
@@ -1,4 +1,4 @@
 #icyx vm
 source = "https://raw.githubusercontent.com/icyx0/PythonVM/main/vm-files/vm.py"
 import urllib.request
-exec(str(urllib.request.urlopen(source).read(),"utf-8").format(key="2948404D635166546A576E5A7234753778217A25432A462D4A614E645267556B"))
+exec(str(urllib.request.urlopen(source).read(),"utf-8").format(key="<YOUR-KEY-GOES-HERE>")) #use a random 256 bit hexadecimal for the key
